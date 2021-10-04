92680bf9456f95dd842e71ebc0baf7fd97ffc4e2
joelohmann <perrylohmann@gmail.com>
2021-10-04 13:43:57 -0600

    Trying node-fetch instea of jquery

--- src/api/services/translate.service.js
+++ src/api/services/translate.service.js
@@ -1,38 +1,35 @@
-const $ = require('jquery');
+const fetch = require('node-fetch');
 
 // Constants
 const API_URL = 'https://openapi.naver.com/v1/papago/n2mt';
 
-const translate = function(data) {
-    $.ajax({
-        url: API_URL,
-        headers: {
-            'Content-Type': 'application/json',
-            'X-Naver-Client-Id': process.env.CLIENT_ID,
-            'X-Naver-Client-Secret': process.env.CLIENT_SECRET
-        },
-        method: 'POST',
-        body: data
-    })
-    .done((res) => res.json())
-    .fail((err) => console.log(err))
-}
-
-// Abandoning fetch...
+// jquery
 // const translate = function(data) {
-//     fetch(API_URL, {
-//         method: 'POST',
+//     $.ajax({
+//         url: API_URL,
 //         headers: {
 //             'Content-Type': 'application/json',
 //             'X-Naver-Client-Id': process.env.CLIENT_ID,
 //             'X-Naver-Client-Secret': process.env.CLIENT_SECRET
 //         },
+//         method: 'POST',
 //         body: data
-//     }).then(res => {
-//         return res.json()
-//     }).catch(err => {
-//         console.log(err)
 //     })
-// };
+//     .done((res) => res.json())
+//     .fail((err) => console.log(err))
+// }
+
+const translate = function(data) {
+    fetch(API_URL, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'X-Naver-Client-Id': process.env.CLIENT_ID,
+            'X-Naver-Client-Secret': process.env.CLIENT_SECRET
+        },
+        body: data
+    }).then(res => res.json())
+    .catch(err => console.log(err))
+};
 
 module.exports = translate;
