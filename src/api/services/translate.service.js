91bd0ca910635ea02eae9ef36903e8c521ad11ab
joelohmann <perrylohmann@gmail.com>
2021-10-04 10:56:46 -0600

    Switched request method from fetch to jQuery.ajax

--- src/api/services/translate.service.js
+++ src/api/services/translate.service.js
@@ -1,22 +1,38 @@
-import fetch from 'node-fetch';
+const $ = require('jquery');
 
 // Constants
 const API_URL = 'https://openapi.naver.com/v1/papago/n2mt';
 
 const translate = function(data) {
-    fetch(API_URL, {
-        method: 'POST',
+    $.ajax({
+        url: API_URL,
         headers: {
             'Content-Type': 'application/json',
             'X-Naver-Client-Id': process.env.CLIENT_ID,
             'X-Naver-Client-Secret': process.env.CLIENT_SECRET
         },
+        method: 'POST',
         body: data
-    }).then(res => {
-        return res.json()
-    }).catch(err => {
-        console.log(err)
     })
-};
+    .done((res) => res.json())
+    .fail((err) => console.log(err))
+}
+
+// Abandoning fetch...
+// const translate = function(data) {
+//     fetch(API_URL, {
+//         method: 'POST',
+//         headers: {
+//             'Content-Type': 'application/json',
+//             'X-Naver-Client-Id': process.env.CLIENT_ID,
+//             'X-Naver-Client-Secret': process.env.CLIENT_SECRET
+//         },
+//         body: data
+//     }).then(res => {
+//         return res.json()
+//     }).catch(err => {
+//         console.log(err)
+//     })
+// };
 
 module.exports = translate;
