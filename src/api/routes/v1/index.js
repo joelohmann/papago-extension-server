c533fdd92c3c63a3ec99f6444e4ef1e84f036f8d
joelohmann <perrylohmann@gmail.com>
2021-10-05 10:26:03 -0600

    Removed testing routes and comments

--- src/api/routes/v1/index.js
+++ src/api/routes/v1/index.js
@@ -1,23 +1,12 @@
 const express = require('express');
-// TODO Uncomment
-// const translate = require('../../services/translate.service.js');
+const translate = require('../../services/translate.service.js');
 
 const router = express.Router();
 
 // TODO Add authentication in each route
-// router.route('/translate').post((req, res) => {
-//     console.log(req.body);
-//
-//     let translation = translate(req.body);
-//     console.log(translation);
-//     res.status(200).send(translation);
-// });
-
-// TODO remove this
-router.route('/test').get((req, res) => {
-    res.status(200).send({
-        test: 'hello'
-    })
+router.route('/translate').post((req, res) => {
+    translate(JSON.stringify(req.body))
+    .then((data) => res.status(200).send(data))
 });
 
 module.exports = router;
