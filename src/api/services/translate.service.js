7f426394642c7971584cf5e4d4f8d7e851a1f60d
joelohmann <perrylohmann@gmail.com>
2021-10-05 10:25:32 -0600

    Removed jquery comments. Changed service to async

--- src/api/services/translate.service.js
+++ src/api/services/translate.service.js
@@ -3,33 +3,18 @@ const fetch = require('node-fetch');
 // Constants
 const API_URL = 'https://openapi.naver.com/v1/papago/n2mt';
 
-// jquery
-// const translate = function(data) {
-//     $.ajax({
-//         url: API_URL,
-//         headers: {
-//             'Content-Type': 'application/json',
-//             'X-Naver-Client-Id': process.env.CLIENT_ID,
-//             'X-Naver-Client-Secret': process.env.CLIENT_SECRET
-//         },
-//         method: 'POST',
-//         body: data
-//     })
-//     .done((res) => res.json())
-//     .fail((err) => console.log(err))
-// }
-
-const translate = function(data) {
-    // fetch(API_URL, {
-    //     method: 'POST',
-    //     headers: {
-    //         'Content-Type': 'application/json',
-    //         'X-Naver-Client-Id': process.env.CLIENT_ID,
-    //         'X-Naver-Client-Secret': process.env.CLIENT_SECRET
-    //     },
-    //     body: data
-    // }).then(res => res.json())
-    // .catch(err => console.log(err))
+async function translate(data) {
+    let response = await fetch(API_URL, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'X-Naver-Client-Id': process.env.CLIENT_ID,
+            'X-Naver-Client-Secret': process.env.CLIENT_SECRET
+        },
+        body: data
+    });
+    let body = await response.json();
+    return body;
 };
 
 module.exports = translate;
