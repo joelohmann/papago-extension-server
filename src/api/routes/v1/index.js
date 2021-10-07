cc0d0be307d1c5eee6913718d2f4f6bdd617493d
joelohmann <perrylohmann@gmail.com>
2021-10-07 09:52:45 -0600

    Added a /api/v1/status route

--- src/api/routes/v1/index.js
+++ src/api/routes/v1/index.js
@@ -1,7 +1,17 @@
 const express = require('express');
+
 const translate = require('../../services/translate.service.js');
 
 const router = express.Router();
+
+router.route('/status').get((req, res) => {
+    res.send({
+        message: "OK",
+        time: new Date().toISOString(),
+        IP: req.ip,
+        URL: req.originalURL
+    });
+});
 
 // TODO Add authentication in each route
 router.route('/translate').post((req, res) => {
