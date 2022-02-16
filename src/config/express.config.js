21086580e7cb3277aad2f6235bb7388d11f6384d
joelohmann <perrylohmann@gmail.com>
2022-02-16 11:26:18 -0700

    Including rate limiting

--- src/config/express.config.js
+++ src/config/express.config.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const limiter = require('../api/middlewares/rate.limiting.js')
 const routes = require('../api/routes/v1');
 
 const app = express();
+
+// Rate limiting
+app.use(limiter);
+app.set('trust proxy', 1);
 
 // Parse body parameters
 app.use(express.json());
