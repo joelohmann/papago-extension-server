af97f65955a083bd4f1e4524ab8335ccac26d5c2
joelohmann <perrylohmann@gmail.com>
2022-02-16 17:38:09 -0700

    Implemented logger Staging for SIGTERM handling

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 const { port, env } = require('./constants')
-const app = require('./config/express.config');
+const app = require('./config/express-config.js');
+const logger = require('./utils/logger.js');
+// const process = require('process');
+// require(logHandler)
+
 
 // Declaring port. Heroku generally will choose its own port
 app.listen(port, (err) => {
     if (err) {
+        logger.error(err);
         return console.log(err);
     }
-        return console.log(`server started [env, port] = [${env}, ${port}]`)
+    
+    logger.info(`server started [env, port] = [${env}, ${port}]`);
+    return console.log(`server started [env, port] = [${env}, ${port}]`)
 });
+
+// Handler for offloading logs before Heroku 
+// process.on('SIGTERM', logHandler);
