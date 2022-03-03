f9d3412d9674e29a6f6429c29368bc4f48b3e6b3
joelohmann <perrylohmann@gmail.com>
2022-03-03 10:15:27 -0700

    Using new logger and prepping to catch SIGTERM

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 const { port, env } = require('./constants')
 const app = require('./config/express-config.js');
 const logger = require('./utils/logger.js');
-// const process = require('process');
-// require(logHandler)
+const sigtermHandler = require('./utils/sigterm-handler');
 
 
 // Declaring port. Heroku generally will choose its own port
@@ -11,10 +10,10 @@ app.listen(port, (err) => {
         logger.error(err);
         return console.log(err);
     }
-    
-    logger.info(`server started [env, port] = [${env}, ${port}]`);
-    return console.log(`server started [env, port] = [${env}, ${port}]`)
+
+    return logger.info(`server started [env, port] = [${env}, ${port}]`);
 });
 
 // Handler for offloading logs before Heroku 
-// process.on('SIGTERM', logHandler);
+// process.on('SIGTERM', sigtermHandler);
+process.on('SIGINT', sigtermHandler);
