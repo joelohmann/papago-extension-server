436dcf6855fcd90c34ce05390c7e122d767bbcc2
joelohmann <perrylohmann@gmail.com>
2022-03-07 14:14:12 -0700

    Connection is only used once, so there isn't much point in worrying about disconnects

--- src/utils/mysql.js
+++ src/utils/mysql.js
@@ -1,5 +1,7 @@
 const mysql = require('mysql');
 
+var connection;
+
 var db_config = {
   host: process.env.SQL_HOST,
   user: process.env.SQL_USER,
@@ -7,27 +9,6 @@ var db_config = {
   database: process.env.SQL_DATABASE
 }
 
-function handleDisconnect() {
-  // Recreate connection
-  connection = mysql.createConnection(db_config);
+connection = mysql.createConnection(db_config);
 
-  // Try reconnecting again if there is an error
-  connection.connect(err => {              
-    if (err) {                                  
-      logger.error('error when connecting to db:', err);
-      setTimeout(handleDisconnect, 2000); 
-    }                                     
-  });                                     
-             
-  // Handle 'connection lost' error (reconnect)
-  connection.on('error', err => {
-    logger.error('db error', err);
-    if (err.code === 'PROTOCOL_CONNECTION_LOST') { 
-      handleDisconnect();                         
-    } else {                                      
-      throw err;                                 
-    }
-  });
-}
-
-module.exports = handleDisconnect;
+module.exports = connection;
