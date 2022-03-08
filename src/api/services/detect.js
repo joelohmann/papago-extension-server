b34de14fc1ec0f0e327ee01369d86f7a6df75f2f
joelohmann <perrylohmann@gmail.com>
2022-03-08 12:31:57 -0700

    This now handles both API calls to Naver Added error logging

--- src/api/services/detect.js
+++ src/api/services/detect.js
@@ -1,19 +1,66 @@
 const fetch = require('node-fetch');
+const logger = require('../../utils/logger');
 
-const API_URL = 'https://openapi.naver.com/v1/papago/detectLangs';
+const DETECT_URL = 'https://openapi.naver.com/v1/papago/detectLangs';
+const TRANSLATE_URL = 'https://openapi.naver.com/v1/papago/n2mt';
 
-async function detect(data) {
-    let response = await fetch(API_URL, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'X-Naver-Client-Id': process.env.CLIENT_ID,
-            'X-Naver-Client-Secret': process.env.CLIENT_SECRET
-        },
-        body: data
-    });
-    let body = await response.json();
-    return body;
+async function detect(body) {
+    try {
+        let detectBody = {
+            "query": body.text
+        }
+
+        let detectResponse = await fetch(DETECT_URL, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'X-Naver-Client-Id': process.env.CLIENT_ID,
+                'X-Naver-Client-Secret': process.env.CLIENT_SECRET
+            },
+            body: JSON.stringify(detectBody)
+        });
+        let detectJSON = await detectResponse.json();
+        let langCode = detectJSON.langCode;
+
+        if (!detectResponse.ok) {
+            logger.error("Naver API call failed");
+
+            let status = detectResponse.status;
+            let data = detectJSON;
+            return {status, data}
+        }
+
+        if (langCode == body.target) {
+            body.target = langCode != 'en' ? 'en' : 'ko';
+        }
+
+        let transBody = {
+            'source': langCode,
+            'target': body.target,
+            'text': body.text,
+            'honorific': body.honorific
+        }
+
+        let transResponse = await fetch(TRANSLATE_URL, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'X-Naver-Client-Id': process.env.CLIENT_ID,
+                'X-Naver-Client-Secret': process.env.CLIENT_SECRET
+            },
+            body: JSON.stringify(transBody)
+        });
+
+        let status = transResponse.status;
+        let data = await transResponse.json();
+
+        return {status, data}
+    } catch (err) {
+        logger.error(err);
+
+        let status = err.status;
+        return {status, err};
+    }
 };
 
 module.exports = detect;
