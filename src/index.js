92c6f8c744385754c1c551a0a3e7e866e14debbb
joelohmann <perrylohmann@gmail.com>
2022-10-25 17:11:29 -0600

    server is no longer sending SIGTERM signals

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 const { port, env } = require('./constants')
 const app = require('./config/express-config.js');
 const logger = require('./utils/logger.js');
-const sigtermHandler = require('./utils/sigterm-handler');
 
 
 // Declaring port. Heroku generally will choose its own port
@@ -13,11 +12,3 @@ app.listen(port, (err) => {
 
     return logger.info(`server started [env, port] = [${env}, ${port}]`);
 });
-
-if (env === 'production') {
-    // Handler for offloading logs before Heroku 
-    process.on('SIGTERM', sigtermHandler);
-}
-
-// Heroku uses SIGTERM, but use SIGINT for debugging
-// process.on('SIGINT', sigtermHandler);
