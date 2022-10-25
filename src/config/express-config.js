bd4cdd20696ce7a77c0d9bbbea6b69852583949e
joelohmann <perrylohmann@gmail.com>
2022-10-25 17:13:40 -0600

    daily scheduled task to switch APIs at the time of the quota limit reset logs also being uploaded at this time every day

--- src/config/express-config.js
+++ src/config/express-config.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const CronJob = require('cron').job;
 
 const limiter = require('../api/middlewares/rate-limiting.js')
+const uploadLogs = require('../utils/upload-logs.js')
 const routes = require('../api/routes/v1');
 
 const app = express();
@@ -8,6 +10,18 @@ const app = express();
 // Rate limiting
 app.use(limiter);
 app.set('trust proxy', 1);
+
+// Scheduled task at 9:05AM Mountain Time every day, the time at which the Naver API limit resets.
+CronJob(
+  '0 5 9 * * *',
+  () => {
+    app.disable('quota exceeded');
+    uploadLogs();
+  },
+  null,
+  true,
+  'America/Denver'
+);
 
 // Parse body parameters
 app.use(express.json());
